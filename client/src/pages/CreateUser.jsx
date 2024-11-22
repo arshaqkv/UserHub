a7ef9b50e636f23f6628914e990ea84f49ed1717
MUHAMMED ARSHAQ K V <83214330+arshaqkv@users.noreply.github.com>
2024-11-22 19:31:03 +0530

    Update CreateUser.jsx

--- client/src/pages/CreateUser.jsx
+++ client/src/pages/CreateUser.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { createUser } from '../features/adminSlice';
+import { addUser } from '../features/adminSlice';
 import './CreateUser.css'
 import { showToast } from '../utils/toastUtils';
 import AdminSidebar from '../components/AdminSidebar';
